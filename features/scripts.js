4bb02a3e80d6a78a2d6ea79a56619851cccab337
Feisal Ahmed <feisala132@gmail.com>
2024-09-13 13:50:42 +0100

    Adding func to create elements and some fonts styling

--- features/scripts.js
+++ features/scripts.js
@@ -1,12 +1,31 @@
+const cardsContainer = document.getElementById('cards-container');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const currentCardSpan = document.getElementById('current-card');
+const totalCardsSpan = document.getElementById('total-cards');
+let currentCardIndex = 0;
 
-const prev = document.getElementById('prev')
 
-const cardBody = document.querySelector()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+// function to create a card element
+function createCard(frontText, backText) {
+    const cardWrapper = document.createElement('div');
+    cardWrapper.classList.add('card-wrapper');
 
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
 
+    const cardFront = document.createElement('div');
+    cardFront.classList.add('card-front');
+    cardFront.innerHTML = `<p>${frontText}</p>`;
 
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('card-back');
+    cardBack.innerHTML = `<p>${backText}</p>`;
 
+    cardBody.appendChild(cardFront);
+    cardBody.appendChild(cardBack);
+    cardWrapper.appendChild(cardBody);
 
-let flashcards = [
-    
-]
+    return cardWrapper;
+  }
